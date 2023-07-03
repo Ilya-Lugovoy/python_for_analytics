742edc801d065c4dafd33981a5355f1c3b63cd04
Ilya <ilya.lugovoy012@gmail.com>
2023-07-03 17:13:51 +0300

    Task 1

---
+++ main.py
@@ -0,0 +1,13 @@
+# Задача 26: Напишите программу, которая на вход принимает два числа A и B,
+# и возводит число А в целую степень B с помощью рекурсии
+
+a = int(input('Enter number a: '))
+b = int(input('Enter number b: '))
+
+def rec_1(a, b, degree_numb=1):
+    if b == 0:
+        return degree_numb
+    b -= 1
+    return rec_1(a, b, degree_numb*a)
+
+print(rec_1(a, b))
