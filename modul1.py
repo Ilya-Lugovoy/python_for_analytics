06e6953d42f2f8e738b4a282d65b017787ccdff6
Ilya <ilya.lugovoy012@gmail.com>
2023-07-19 10:36:21 +0300

    Task 1

--- modul1.py
+++ modul1.py
@@ -1,27 +1,11 @@
-# import random
-# def generate_list(len_list):
-#     list_res = []
-#     for i in range(len_list):
-#         list_res.append(random.randint(1, 10))
-    
-#     return list_res
+import random
+import math
+# list_res = []
+# for i in range(9):
+#     list_res.append(random.randint(1, 10)) # Создаёт случайное число в выбранном диапазоне
+# print(list_res)
 
-# def find_minus_list(len_1, len_2):
-#     list_end = []
-#     for item in len_1:
-#         if item not in len_2:
-#             list_end.append(item)
-
-#     return list_end
-
-# len_1 = int(input('Enter len of list 1: '))
-# list_1 = generate_list(len_1)
-# print(list_1)
-# len_2 = int(input('Enter len of list 2: '))
-# list_2 = generate_list(len_2)
-# print(list_2)
-
-# list_end = find_minus_list(list_1, list_2)
-# print(list_end)
-
-# list_2 = [int(input(f'Введите {i+1} эллемент второго списка')) for i in range(num2)]
+# list_of_orbits = [(1, 3), (6, 6), (7, 2), (2.5, 10), (10, 1)]
+# areas = [(math.pi * a * b if a != b else 0) for a, b in list_of_orbits]
+# max_area_index = areas.index(max(areas)) # max() - находит максимальное значение из коллекции; col.index() - определяет индекс элемента коллекции
+# print(list_of_orbits[max_area_index])
