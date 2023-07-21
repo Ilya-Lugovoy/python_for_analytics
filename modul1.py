d2caebd0b0756575af10b03c2eeae2f0d5a0aaaa
Ilya <ilya.lugovoy012@gmail.com>
2023-07-21 10:50:39 +0300

    Phone book

--- modul1.py
+++ modul1.py
@@ -1,5 +1,7 @@
 import random
 import math
+import pathlib
+from pathlib import Path
 # list_res = []
 # for i in range(9):
 #     list_res.append(random.randint(1, 10)) # Создаёт случайное число в выбранном диапазоне
@@ -9,3 +11,10 @@ import math
 # areas = [(math.pi * a * b if a != b else 0) for a, b in list_of_orbits]
 # max_area_index = areas.index(max(areas)) # max() - находит максимальное значение из коллекции; col.index() - определяет индекс элемента коллекции
 # print(list_of_orbits[max_area_index])
+
+
+# file_path = Path('gb_py', 'new_main.py')
+# print(file_path)
+# with open(file_path, 'r') as f:
+#     for i, line in enumerate(f):
+#         print(i, line.strip())
