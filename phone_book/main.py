988e996eb4c94d02de59e0465dc2bd7a37926522
Ilya <ilya.lugovoy012@gmail.com>
2023-07-21 14:36:53 +0300

    Phone book 3.0

--- phone_book/main.py
+++ phone_book/main.py
@@ -3,10 +3,18 @@ from pathlib import Path
 FILE_PATH = Path('gb_py', 'phone_book', 'phone_book.txt')
 print(FILE_PATH)
 
+def count_id():
+    with open(FILE_PATH, 'r', encoding='utf8') as file:
+        contact_id = 1
+        for line in file:
+            contact_id += 1
+        return contact_id
+
 def add_contact():
-    with open(FILE_PATH, 'a', encoding = 'utf8') as file:
-        info = input('Enter data according to the sample - Last name First name;phone number: ')
-        file.write(f'\n{info}')
+    with open(FILE_PATH, 'a', encoding='utf8') as file:
+        info = input('Enter data according to the sample - Last name First name; phone number: ')
+        contact_id = count_id()
+        file.write(f'\n{contact_id}. {info}')
 
 def show_contact():
     with open(FILE_PATH, 'r', encoding = 'utf8') as file:
@@ -22,6 +30,32 @@ def find_contact():
             if search in contact:
                 list_1.append(contact)
         print(*(line for line in list_1))
+
+def change_contact():
+    show_contact()
+    with open(FILE_PATH, 'r+', encoding='utf8') as file:
+        n = int(input('Enter a contact number to change it: '))
+        lines = file.readlines()
+        if n > len(lines):
+            print('Invalid contact number')
+            return
+        new_info = input('Enter data according to the sample - Last name First name; phone number: ')
+        lines[n-1] = f'{n}. {new_info}\n'
+        file.seek(0)
+        file.writelines(lines)
+
+def delete_contact():
+    show_contact()
+    with open(FILE_PATH, 'r+', encoding='utf8') as file:
+        n = int(input('Enter a contact number to delete it: '))
+        lines = file.readlines()
+        if n > len(lines):
+            print('Invalid contact number')
+            return
+        del lines[n-1]
+        file.seek(0)
+        file.truncate(0)
+        file.writelines(lines)
     
 def choose():
     flag=True
@@ -30,7 +64,9 @@ def choose():
         1 - add contact \
         2 - show contacts \
         3 - find contact \
-        4 - finish session')
+        4 - change contact \
+        5 - delete contact \
+        6 - finish session')
         n = input('Enter option: ')
         match n:
             case '1':
@@ -40,6 +76,10 @@ def choose():
             case '3':
                 find_contact()
             case '4':
+                change_contact()
+            case '5':
+                delete_contact()
+            case '6':
                 flag=False
 
 choose()
